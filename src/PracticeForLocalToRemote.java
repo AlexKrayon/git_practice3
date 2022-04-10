9aef9c97b749498c282a5ccc8ccc0b9e6c4ddac7
Alex <fatihbalci.phd@gmail.com>
2022-04-10 12:53:34 +0300

    commit1

---
+++ src/PracticeForLocalToRemote.java
@@ -0,0 +1,11 @@
+public class PracticeForLocalToRemote {
+    public static void main(String[] args) {
+        System.out.println("We learn new subjects.");
+
+        String name = "Ahmet";
+        int age = 23;
+
+
+
+    }
+}
